b8990ec5b8a0d1f926b9be75a57ef8c0348d99a3
Serhii <0992830717s@gmail.com>
2024-11-15 16:54:53 +0100

    Fraction+

--- NET.DZ7/Program.cs
+++ NET.DZ7/Program.cs
@@ -1,15 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NET.DZ7
+﻿internal class Program
 {
-    internal class Program
+    struct Fraction
     {
-        static void Main(string[] args)
+        public int numerator;
+        public int denominator;
+        public int NOD(int x, int y)
         {
+            if (y == 0) return x;
+            return NOD(y, x % y);
         }
+        public void input(int c, int z)
+        {
+            numerator = c;
+            denominator = z;
+        }
+        public  void mult(int x)
+        {
+            numerator *= x;
+            Console.WriteLine( "Fraction:  " + numerator+'/'+ denominator );
+        }
+        public  void div(int x)
+        {
+            denominator *= x;
+            Console.WriteLine("Fraction:  " + numerator + '/' + denominator); 
+
+        }
+
+        public void sum(int x)
+        {
+            numerator += (x * denominator);
+            Console.WriteLine("Fraction:  " + numerator + '/' + denominator);
+        }
+        public  void sub(int x)
+        {
+            numerator -= (x * denominator);
+            Console.WriteLine("Fraction:  " + numerator + '/' + denominator);
+
+        }
+        public void show()
+        {
+            Console.WriteLine("Fraction:  " + numerator + '/' + denominator);
+        }
+        public  int peredacha()
+        {
+            return NOD(numerator, denominator);
+        }
+        public  void reduct(int x)
+        {
+            numerator /= x;
+            denominator /= x;
+        }
+
     }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
 }
